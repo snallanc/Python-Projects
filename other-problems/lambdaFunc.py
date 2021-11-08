fb7d33fb3aeac9e2237f3629d234df7c96fd3145
snallanc <ncsudarshan.1987@gmail.com>
2021-11-08 06:10:41 -0500

    added the output for lambda exmaple

--- other-problems/lambdaFunc.py
+++ other-problems/lambdaFunc.py
@@ -11,3 +11,19 @@ p=power(10)
 print("Powers of 10:\n")
 for i in range(1,10):
   print(p(i))
+
+"""
+Output:
+
+Powers of 10:
+
+10
+100
+1000
+10000
+100000
+1000000
+10000000
+100000000
+1000000000
+"""
