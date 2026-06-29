428590b4d322045b6297c39ea81543ff1aa23ed3
Sudarshan NC <ncsudarshan.1987@gmail.com>
2026-06-29 15:31:45 +0530

    Fix a bug in reverse_endian.py and add test ccode

--- algorithms/interview_problems/bit_manipulation/reverse_endian.py
+++ algorithms/interview_problems/bit_manipulation/reverse_endian.py
@@ -1,11 +1,17 @@
 def reverse_endian_32bit(n):
-    return ((n & 0xFF000000 >> 24) |
-           (n & 0xFF000000 >> 8 ) |
-           (n & 0xFF00 << 8) |
-           (n & 0xFF << 24))
+    return ((n & 0xFF000000) >> 24) | \
+           ((n & 0xFF0000) >> 8 ) | \
+           ((n & 0xFF00) << 8) | \
+           ((n & 0xFF) << 24)
 
 def reverse_endian_pythonic(n):
     bytes = n.to_bytes(4, 'big')
     #return int.from_bytes(bytes[::-1], 'big')
     #OR
     return int.from_bytes(bytes, 'little')
+
+"""Test code"""
+if __name__ == "__main__":
+    test_values = [0x12345678, 0xAABBCCDD, 0x0, 0xFFFFFFFF, 0x01020304, 0xDEADBEEF]
+    for val in test_values:
+        print(f"Input: {hex(val)} -> Output: {hex(reverse_endian_32bit(val))}, Pythonic: {hex(reverse_endian_pythonic(val))}")
